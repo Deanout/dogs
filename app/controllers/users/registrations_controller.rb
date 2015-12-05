126e6443dd3bd3a3fa7d880fe644210a810c6cf5
Dean DeHart <dddehart@oakland.edu>
2015-12-05 14:36:42 -0500

    Done with pictures?

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, :only => [:create]
+
   protected
+
   def after_sign_up_path_for(resource)
     signed_in_root_path(resource)
   end
@@ -12,8 +15,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def registration_params
+    params.require(:user).permit(:name, :email,:password, :password_confirmation)
   end
 
   def account_update_params
