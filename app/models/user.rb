e4e0b2043614cf135ad152a6d2bacd1b4219f708
Dean DeHart <dddehart@oakland.edu>
2015-12-06 07:11:08 -0500

    Heroku fix?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         has_many :pins
-         has_many :litters
-         has_many :comments
+    :recoverable, :rememberable, :trackable, :validatable
+  has_many :pins
+  has_many :litters
+  has_many :comments
+
+  ADMIN = {
+    0 => false,
+    1 => true,
+  }
 end
