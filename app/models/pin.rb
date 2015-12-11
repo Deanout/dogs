19e07cbf87937d80c760c2664b638d87fb487b80
Dean DeHart <dddehart@oakland.edu>
2015-12-11 14:53:51 -0500

    Commited via pdeanout command

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, styles: {medium: "300x300>"},
-	:path => ":rails_root/public/"
+	has_attached_file :image, styles: {medium: "300x300>"},url: "/system/:hash.:extension",
+                  hash_secret: "abc123"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
